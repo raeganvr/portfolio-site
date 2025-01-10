97e42e36dfae24a316dba821acec15dcf0836926
Raegan <raeganvr7@gmail.com>
2025-01-10 11:01:48 -0800

    figuring our adding photos in a unique way

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Image from "next/image";
-import React, { useEffect } from 'react';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { IoPin } from 'react-icons/io5';
 import { MdEmail } from "react-icons/md";
@@ -72,64 +71,126 @@ function NavigationBar() {
 function HeroSection() {
   return (
     <section id="hero" className="py-20 px-16 bg-black-100">
-      <div className="border border-white/70 p-8 rounded-lg shadow-md grid grid-cols-1 sm:grid-cols-2 items-center gap-16 transition-transform duration-300 hover:shadow-lg hover:scale-105 mx-auto max-w-[800px]">
+    <div className="border border-white/70 p-8 rounded-lg shadow-md grid grid-cols-1 sm:grid-cols-2 items-center gap-16 transition-transform duration-300 hover:shadow-lg hover:scale-105 mx-auto max-w-[800px] animate-glow">
+      {/* Left Content: Name and Description */}
+      <div className="flex flex-col">
+        <h2 className="text-2xl font-bold text-white text-left">Raegan Van Raamsdonk</h2>
+
+        <p className="text-md font-bold text-gray-400 text-left">Computer Science Student</p>
+        <p className="text-sm text-gray-400 text-left flex items-center">
+          <IoPin /> British Columbia, Canada
+        </p>
+        <p className="text-sm text-gray-300 text-left py-4">
+          I am a computer science student who enjoys creating games,
+          full-stack web applications, and solving data structure and algorithms problems.
+        </p>
+
+        {/* Social Buttons */}
+        <div className="flex gap-4">
+          <a
+            href="https://github.com/raeganvr"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors"
+          >
+            <FaGithub size={20} /> GitHub
+          </a>
+          <a
+            href="https://www.linkedin.com/in/raeganvr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 bg-blue-700 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors"
+          >
+            <FaLinkedin size={20} /> LinkedIn
+          </a>
+          <a
+            href="mailto:john@example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors"
+          >
+            <MdEmail size={20} /> Email
+          </a>
+        </div>
+      </div>
+
+      {/* Right Content: Portrait */}
+      <div className="flex justify-center">
+        <Image
+          src="/images/portrait.jpg"
+          alt="Portrait of Raegan Van Raamsdonk"
+          width={324}
+          height={246}
+          className="rounded-full object-cover"
+          priority // Optimized image loading
+        />
+      </div>
+    </div>
+  </section>
+  );
+}
+
+function AboutMe(){
+  return (
+    <section id="about" className="py-20 flex flex-col items-center gap-8">
+      <h2 className="text-3xl font-bold text-white-800">About Me</h2>
+
+      <div className="border border-white/70 p-8 shadow-md grid grid-cols-1 sm:grid-cols-2 items-center gap-8 transition-transform duration-300 hover:shadow-lg hover:scale-105 mx-auto max-w-[1100px] animate-glow">
+        {/*left pannel text*/}
+        <div className="space-y-8">
+          <p className="text-2xl text-gray-100 text-left font-bold">
+            Who I Am
+          </p>
+          <p className="text-md text-gray-300 text-left">
+            Hi, I&apos;m Raegan Van Raamsdonk, a student with a love
+            for math, computer science, and problem-solving. 
+            I enjoy exploring the logic and creativity behind 
+            coding, whether it&apos;s building web applications, 
+            creating games, or tackling algorithm challenges. 
+            Outside of academics, I love weight training, ultimate 
+            frisbe, soccer, and chess. My goal is to combine my 
+            passions and skills to create solutions and 
+            continue learning every step of the way.
+          </p>
+        </div>
         
-        {/* Left Content: Name and Description */}
-        <div className="flex flex-col">
-          <h2 className="text-2xl font-bold text-white text-left "> Raegan Van Raamsdonk</h2>
+        {/* Right: Overlapping Photos */}
+        <div className="relative w-[400px] h-[400px]">
+          {/* First Photo */}
+          <div
+            className="absolute top-0 left-0 z-0"
+            style={{ width: '300px', height: '300px' }}
+          >
+            <Image
+              src="/images/dominican.jpeg"
+              alt="Backflip in Dominican Republic"
+              width={150}
+              height={150}
+              className="rounded-lg shadow-lg"
+            />
+          </div>
 
-          <p className="text-md font-bold text-gray-400 text-left">Computer Science Student</p>
-          <p className="text-sm text-gray-400 text-left flex items-center">
-            <IoPin /> British Columbia, Canada
-          </p>
-          <p className="text-sm text-gray-300 text-left py-4">
-            I am a computer science student who enjoys creating games,
-            full-stack web applications, and solving data structure and algorithms problems.
-          </p>
-
-          {/* Social Buttons */}
-          <div className="flex gap-4">
-            <a
-              href="https://github.com/raeganvr"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors"
-            >
-              <FaGithub size={20} /> GitHub
-            </a>
-            <a
-              href="https://www.linkedin.com/in/raeganvr/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-blue-700 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors"
-            >
-              <FaLinkedin size={20} /> LinkedIn
-            </a>
-            <a
-              href="mailto:john@example.com"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 bg-gray-800 text-white py-2 px-4 rounded-lg hover:bg-gray-700 transition-colors"
-            >
-              <MdEmail size={20}/> Email
-            </a>
+          {/* Second Photo */}
+          <div
+            className="absolute top-40 left-32 z-10"
+            style={{ width: '300px', height: '300px' }}
+          >
+            <Image
+              src="/images/sooke.jpeg"
+              alt="French Beach in Sooke, BC"
+              width={150}
+              height={150}
+              className="rounded-lg shadow-md"
+            />
           </div>
         </div>
 
-        {/* Right Content: Portrait */}
-        <div className="flex justify-center">
-          <Image
-            src="/images/portrait.jpg"
-            alt="Portrait of Raegan Van Raamsdonk"
-            width={324}
-            height={246}
-            className="rounded-full object-cover"
-            priority // Optimized image loading
-          />
-        </div>
+
+        
       </div>
+      
     </section>
-  );
+  )
 }
 
 // Projects Section Component
@@ -179,14 +240,16 @@ function Footer() {
 export default function Home() {
   return (
     <div className="relative min-h-screen">
-      <div className="absolute inset-0 bg-black bg-stars z-0"></div>
-      <div className="absolute inset-0 bg-twinkle z-10 animate-twinkle"></div>
+      <div className="absolute inset-0 bg-black z-0"></div>
+      <div className="absolute inset-0 bg-stars bg-fixed z-10"></div>
+      <div className="absolute inset-0 bg-twinkle bg-fixed z-20 animate-twinkle"></div>
 
       {/* Content */}
       <div className="relative z-20">
         <NavigationBar />
         <main className="flex-grow">
           <HeroSection />
+          <AboutMe />
           <ProjectsSection />
           <ContactSection />
         </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,12 +13,17 @@ export default {
         twinkle: "url('/images/twinkling.png')",
       },
       animation: {
-        twinkle: "move-twinkle 200s linear infinite",
+        twinkle: "move-twinkle 300s linear infinite",
+        glow: "glow 5s infinite",
       },
       keyframes: {
         "move-twinkle": {
           "0%": { backgroundPosition: "0 0" },
           "100%": { backgroundPosition: "-10000px 5000px" },
+        },
+        glow: {
+          "0%, 100%": { boxShadow: "0 0 20px 10px rgba(0, 255, 255, 0.5)" },
+          "50%": { boxShadow: "0 0 30px 15px rgba(0, 255, 255, 0.8)" },
         },
       },
     },
